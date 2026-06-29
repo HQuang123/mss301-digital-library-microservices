a80d7d8dc76d35128dcb614b574bc8bab814049b
PhuNguyen12345 <phukak12345@gmail.com>
2026-06-29 23:43:26 +0700

    feat: add bulkhead pattern.

--- services/catalog-service/src/main/java/fu/edu/mss301/digilib/catalog/api/controller/DemoResillenceController.java
+++ services/catalog-service/src/main/java/fu/edu/mss301/digilib/catalog/api/controller/DemoResillenceController.java
@@ -23,4 +23,19 @@ public class DemoResillenceController {
         Thread.sleep(5000);
         return "Success từ Catalog Service (nhưng rất chậm)";
     }
+
+    // API riêng để test Bulkhead (Chỉ cho phép 1 request vào cùng lúc)
+    @GetMapping("/bulkhead")
+    @io.github.resilience4j.bulkhead.annotation.Bulkhead(name = "catalogBulkhead", fallbackMethod = "bulkheadFallback")
+    public String bulkheadEndpoint() throws InterruptedException {
+        System.out.println("[Catalog] Bắt đầu xử lý khoang tàu Bulkhead vào lúc: " + java.time.LocalTime.now());
+        Thread.sleep(5000); // Ngâm 5 giây để mô phỏng đang bận xử lý
+        return "Success! Khoang tàu Bulkhead đã xử lý xong.";
+    }
+
+    // Hàm Fallback khi Bulkhead đầy (Bị chặn ngay lập tức, không cần chờ)
+    public String bulkheadFallback(Exception e) {
+        System.out.println("[Catalog] Bulkhead đầy! Đã chặn 1 request vào lúc: " + java.time.LocalTime.now());
+        return "Bulkhead Fallback: Hệ thống đang quá tải (hết khoang trống), vui lòng thử lại sau!";
+    }
 }

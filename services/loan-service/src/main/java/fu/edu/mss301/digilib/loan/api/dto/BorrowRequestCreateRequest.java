3b32261873beb0c024dd96f67ce4c4fa122a349a
PhuNguyen12345 <phukak12345@gmail.com>
2026-07-21 17:34:07 +0700

    feat: merge main-fix-loan into main.

--- services/loan-service/src/main/java/fu/edu/mss301/digilib/loan/api/dto/BorrowRequestCreateRequest.java
+++ services/loan-service/src/main/java/fu/edu/mss301/digilib/loan/api/dto/BorrowRequestCreateRequest.java
@@ -3,8 +3,7 @@ package fu.edu.mss301.digilib.loan.api.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record BorrowLoanRequest(
-        @NotNull String memberId,
+public record BorrowRequestCreateRequest(
         @NotNull Long bookId,
         @NotBlank String bookType,
         @NotBlank String idempotencyKey

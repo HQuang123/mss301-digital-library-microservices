69724713cfa63d61a80f16dbbae159546aef6bfe
PhuNguyen12345 <phukak12345@gmail.com>
2026-07-21 17:22:34 +0700

    feat: fix Loan and BorrowedLoanRequest.

--- services/loan-service/src/main/java/fu/edu/mss301/digilib/loan/api/dto/BorrowLoanRequest.java
+++ services/loan-service/src/main/java/fu/edu/mss301/digilib/loan/api/dto/BorrowLoanRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record BorrowLoanRequest(
+        @NotNull String memberId,
         @NotNull Long bookId,
         @NotBlank String bookType,
         @NotBlank String idempotencyKey
